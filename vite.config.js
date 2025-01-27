d745437ee102201da099a34f04bbebd57b095003
[el jomi ibtissam] <[eljomiibtissam@gmail.com]>
2025-01-27 19:38:37 +0100

    ajout

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/fimographie_project/', // Remplacez NOM_DU_REPO par le nom de votre dépôt GitHub
-});
