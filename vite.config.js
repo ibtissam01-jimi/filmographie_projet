5fbdfc0fce128a96435ae741ab3da88b0112a68e
[el jomi ibtissam] <[eljomiibtissam@gmail.com]>
2025-02-04 19:04:44 +0100

    second mod

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"/filmographie_projet/",
-  
+  base: '/filmographie_projet/', // Mets ici le nom exact de ton repo GitHub
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html' // Ajoute cette ligne pour préciser le chemin de index.html
+    }
+  }
 });
